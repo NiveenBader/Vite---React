120d381b28679c2a45669f5ba483b33e83c94fb3
niveenbader <neveen.bader@gmail.com>
2024-09-18 13:26:50 +0300

    first commit

---
+++ real-app-front/src/components/common/pageHeader.jsx
@@ -0,0 +1,12 @@
+function PageHeader({ title, description }) {
+  return (
+    <div className="row  mb-4">
+      <div className="col">
+        <h1>{title}</h1>
+        {description && <div className="col">{description}</div>}
+      </div>
+    </div>
+  );
+}
+
+export default PageHeader;
---
+++ real-app-front/src/components/header.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./navbar.jsx";
+
+function Header() {
+  return (
+    <header>
+      <NavBar />
+    </header>
+  );
+}
+
+export default Header;
